c6d839374d253dadf661f8cda6860f0eb6472137
Arcadia Roleplay <79526162+ArcadiaRoleplay@users.noreply.github.com>
2021-05-16 03:27:33 +0100

    Main Files

---
+++ client/main.lua
@@ -0,0 +1,33 @@
+QBCore = nil
+
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+RegisterNetEvent('qb-items:client:use:duffel-bag')
+AddEventHandler('qb-items:client:use:duffel-bag', function(BagId)
+	local player = PlayerPedId()
+	if not clothingitem then
+    QBCore.Functions.Progressbar("use_bag", "Putting on Bag", 2000, false, true, {
+        disableMovement = false,
+        disableCarMovement = false,
+		disableMouse = false,
+		disableCombat = true,
+    }, {}, {}, {}, function() -- Done
+		RequestAnimDict(dict)
+		TaskPlayAnim(player, "clothingtie", "try_tie_negative_a", 3.0, 3.0, 2000, 51, 0, false, false, false)
+		Wait (600)
+		ClearPedSecondaryTask(PlayerPedId())
+		SetPedComponentVariation(PlayerPedId(), 5, 41, 0, 2)
+		clothingitem = true
+		TriggerServerEvent("inventory:server:OpenInventory", "stash", 'bag_'..BagId, {maxweight = 18000, slots = 9})
+		TriggerEvent("inventory:client:SetCurrentStash", 'bag_'..BagId)
+		end)
+	elseif clothingitem then
+		clothingitem = false
+		RequestAnimDict(dict)
+		TaskPlayAnim(player, "clothingtie", "try_tie_negative_a", 3.0, 3.0, 2000, 51, 0, false, false, false)
+		Wait (600)
+		ClearPedSecondaryTask(PlayerPedId())
+		SetPedComponentVariation(PlayerPedId(), 5, 0, 0, 2)
+	end
+	Citizen.Wait(1000)
+end)
---
+++ server/main.lua
@@ -0,0 +1,10 @@
+QBCore = nil
+
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+QBCore.Functions.CreateUseableItem("duffel-bag", function(source, item)
+	local Player = QBCore.Functions.GetPlayer(source)
+	if Player.Functions.GetItemBySlot(item.slot) ~= nil then
+        TriggerClientEvent('qb-items:client:use:duffel-bag', source, item.info.bagid)
+    end
+end)
