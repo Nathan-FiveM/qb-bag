60662642dce84378a1934b5a4fe45d5c4af4a83d
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-12-20 01:22:41 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-
 QBCore.Functions.CreateUseableItem("duffel-bag", function(source, item)
 	local Player = QBCore.Functions.GetPlayer(source)
 	if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent('qb-items:client:use:duffel-bag', source, item.info.bagid)
+        TriggerClientEvent('qb-bag:client:duffelbag', source, item.info.bagid)
     end
 end)
