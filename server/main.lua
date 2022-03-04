bf873096f8894a6eea386c3ba9e8a1642132645f
Arcadia Roleplay <arcadiaroleplaygtav@gmail.com>
2022-03-04 22:00:20 +0000

    qb-core exports

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-
 QBCore.Functions.CreateUseableItem("duffel-bag", function(source, item)
 	local Player = QBCore.Functions.GetPlayer(source)
 	if Player.Functions.GetItemBySlot(item.slot) ~= nil then
